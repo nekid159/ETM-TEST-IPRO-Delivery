51d3813b8fa1e365eec08f1b3ee558cc75cd7f3e
nekid159 <nekid159@yandex.ru>
2023-11-07 17:21:22 +0300

    all the other classes

--- src/test/java/lib/ui/MainPageObject.java
+++ src/test/java/lib/ui/MainPageObject.java
@@ -66,4 +66,250 @@ public class MainPageObject extends BaseSeleniumPage {
         int dayOfMonthAgain = nextSunday.getDayOfMonth();
         System.out.println(dayOfMonthAgain);
     }
+
+    public WebElement waitForElementAndClickable(String xpath, String error_message, long timeoutInSeconds){
+        WebDriverWait wait=new WebDriverWait(driver, 10);
+        wait.withMessage(error_message + "\n");
+        return wait.until
+                (ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+
+    }
+
+
+    public WebElement waitForElementAndSendKeys(String xpath, String value, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(xpath,error_message,timeoutInSeconds);
+        element.sendKeys(value);
+
+        return element;
+
+    }
+
+    public boolean waitForElementNotPresent(String xpath, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(By.xpath(xpath))
+        );
+    }
+
+    public WebElement waitForElementAndClear(String xpath, String error_message, long timeoutInSeconds) {
+
+        WebElement element = waitForElementPresent(xpath,error_message, timeoutInSeconds);
+        element.clear();
+        return element;
+    }
+    public WebElement waitForElementLocated(String xpath, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+
+        return wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath))
+        );
+    }
+    public void clickVisible(By by ,String error_message, long timeoutInSeconds ){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.until(ExpectedConditions.elementToBeClickable(by));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        final Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(by)).click().perform();
+    }
+    public void assertElementHasText(String xpath, String expected_text, String error_message) {
+
+        long timeoutInSeconds = 0;
+        WebElement element = waitForElementPresent( xpath, error_message,timeoutInSeconds);
+        Assertions.assertTrue(
+                element.getAttribute("text").contains(expected_text),
+                error_message
+        );
+    }
+
+
+
+    public void scrollWebPageUp() {
+
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("window.scrollBy(0,250)");
+    }
+
+    public void scrollWebPageTillElementNotVisible(String xpath,String error_message, int max_swipes) {
+
+        int already_swiped = 0;
+        long timeoutInSeconds = 0;
+        By by = null;
+        WebElement element = this.waitForElementPresent(xpath, error_message,timeoutInSeconds);
+
+        while (!this.isElementLocatedOnTheScreen(xpath)) {
+
+            scrollWebPageUp();
+            already_swiped++;
+            if (already_swiped > max_swipes) {
+                assertTrue(element.isDisplayed(), error_message);
+            }
+        }
+    }
+
+
+
+
+    public int getAmountOfElements(By locator) {
+
+
+        List<?> elements = driver.findElements(locator);
+        return elements.size();
+    }
+
+    public void assertElementNotPresent(By locator, String error_message) {
+
+        int amount_of_elements = getAmountOfElements(locator);
+
+        if (amount_of_elements > 0) {
+            String default_message = "An element '" + locator + "' supposed to be not present";
+            throw new AssertionError(default_message + " " + error_message);
+        }
+    }
+
+    public String waitForElementAndGetAttribute(String xpath, String attribute, String error_message, long timeout_in_seconds) {
+
+        WebElement element = waitForElementPresent(xpath, error_message, timeout_in_seconds);
+        return element.getAttribute(attribute);
+    }
+
+    public void assertElementPresent(By locator, String error_message) {
+
+
+        if (driver.findElements(locator).size() < 1) {
+            String default_message = "An element '" + locator + "' supposed to be present";
+            throw new AssertionError(default_message + " " + error_message);
+        }
+    }
+
+
+
+    private boolean isElementLocatedOnTheScreen(String xpath){
+
+        int element_location_by_y = this.waitForElementPresent(
+                        xpath,
+                        "Cannot find element by locator",
+                        15)
+                .getLocation().getY();
+
+        {
+            JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+            Object js_result = jsExecutor.executeScript("return window.pageYOffset");
+            element_location_by_y -= Integer.parseInt(js_result.toString());
+        }
+        int screen_size_by_y = driver.manage().window().getSize().getHeight();
+
+        return element_location_by_y < screen_size_by_y;
+    }
+
+    public boolean isElementPresent(By locator) {
+        return getAmountOfElements(locator) > 0;
+    }
+
+    public void tryClickElementWithFewAttempts(String xpath, String error_message, int amount_of_attempts) {
+
+        int current_attempts = 0;
+        boolean need_more_attempts = true;
+
+        while (need_more_attempts) {
+            try {
+                this.waitForElementAndClick(
+                        xpath,
+                        error_message,
+                        10
+                );
+                need_more_attempts = false;
+            } catch (Exception exception) {
+                if (current_attempts > amount_of_attempts) {
+                    this.waitForElementAndClick(
+                            xpath,
+                            error_message,
+                            10
+                    );
+                }
+            }
+            current_attempts++;
+        }
+    }
+
+
+
+    protected void clickTrashBinElementToDeleteSavedArticle(String xpath, String error_message) {
+
+        WebElement element = this.waitForElementAndClick(xpath, error_message, 10);
+    }
+
+    public String takeScreenshot(String name) {
+
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        String path = System.getProperty("user.dir") + "/" + name + "_screenshot.png";
+
+        try {
+            FileUtils.copyFile(source, new File(path));
+            System.out.println("The screenshot was taken: " + path);
+        } catch (Exception e) {
+            System.out.println("Cannot take screenshot. Error " + e.getMessage());
+        }
+        return path;
+    }
+
+    @Attachment
+    public static byte[] screenshot(String path) {
+
+        byte[] bytes = new byte[0];
+
+        try {
+            bytes = Files.readAllBytes(Paths.get(path));
+        } catch (IOException e) {
+            System.out.println("Cannot get bytes from screenshot. Error: " + e.getMessage());
+        }
+        return bytes;
+    }
+    public int between() {
+        LocalDate startInclusive = LocalDate.now().plusDays(1);
+        LocalDate endExclusive = LocalDate.now().plusDays(13);
+        long startEpochDay = startInclusive.toEpochDay();
+        long endEpochDay = endExclusive.toEpochDay();
+        long randomDay = ThreadLocalRandom
+                .current()
+                .nextLong(startEpochDay, endEpochDay);
+
+        return LocalDate.ofEpochDay(randomDay).getDayOfMonth();
+    }
+    public void twst() {
+        WebElement webElement = driver.findElement(elements.buttonDateDelivery);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", webElement);
+    }
+
+    public void clickOrderDelivery() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(elements.buttonDateFake));
+        driver.findElement(elements.buttonDateFake).click();
+        if (driver.findElement(elements.buttonDateDelivery).isDisplayed()) {
+            twst();
+        } else {
+            return;
+        }
+        Thread.sleep(1000);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(elements.buttonTimeDelivery));
+        driver.findElement(elements.buttonTimeDelivery).click();
+        if (driver.findElement(elements.buttonEightTimeDelivery).isDisplayed()) {
+            driver.findElement(elements.buttonEightTimeDelivery).click();
+        } else {
+            return;
+        }
+        Thread.sleep(1000);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(elements.buttonTimeEndDelivery));
+        driver.findElement(elements.buttonTimeEndDelivery).click();
+        if (driver.findElement(elements.buttonSixteenTimeDelivery).isDisplayed()) {
+            driver.findElement(elements.buttonSixteenTimeDelivery).click();
+        } else {
+            return;
+        }
+        //wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div/div/div/div/div/div/div/div/div[contains(.,'Стоимость:')]")));
+        //driver.findElement(By.xpath("div[contains(.,'Стоимость: 390 руб.')]")).isDisplayed();
+    }
 }
--- src/test/java/lib/webelements/webelements.java
+++ src/test/java/lib/webelements/webelements.java
@@ -10,5 +10,48 @@ public class webelements {
     lib.ui.MainPageObject MainPageObject = new MainPageObject();
     //покупка
     public By buttonDigineticaMainDescr = By.xpath("//*[@data-testid='diginetica-main-descr-2415539']");
+    public By buttonAgreeCheckbox = By.xpath("//input[@data-testid='checkbox-0']/following::input[3]");
+    public By buttonSaveEstimate = By.xpath("//button[@data-testid='btn-save-as-estimate']");
+    public By buttonDigineticaMainDescr8157448 = By.xpath("//*[@data-testid='diginetica-main-descr-8157448']");
+    public By fieldCntInBasket = By.xpath("//input[@class='MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputMarginDense MuiOutlinedInput-inputMarginDense']");
+    public By buttonAddToBasket = By.xpath("//p[contains(.,'В корзину')]");///button[@data-testid='add_to_basket_button']
+    public By buttonNotistackSnackbar = By.xpath("//div/div[@id='notistack-snackbar']");
+    public By buttonCurrentCity = By.xpath("//button[@data-testid='current-city']");
+    public By buttonClearBasket = By.xpath("//div[@data-testid='action-item-4']");
+    public By buttonYesDelete = By.xpath("//button[@data-testid='confirm-delete-yes']");
+    public By buttonFastOrder = By.xpath("//button[@data-testid='btn-fast-order']");
+    public By buttonOptionDeliveryPickup = By.xpath("//div[@data-testid='option-delivery'][1]");
+    public By buttonOptionDelivery = By.xpath("//div[@data-testid='option-delivery'][2]");
+    public By buttonDelivery = By.xpath("//div/div/div/div/div/div/div/div/div/div[contains(.,'Доставка')]");
+    public By buttonDateFake = By.xpath("//input[@name='date_fake']");
+    public By buttonDateDelivery = By.xpath("//span/p[contains(.,'" + MainPageObject.between() + "')]");
+    public By buttonTimeDelivery = By.xpath("//div[@class='MuiSelect-root MuiSelect-select MuiSelect-selectMenu MuiSelect-outlined MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputMarginDense MuiOutlinedInput-inputMarginDense']");
+    public By buttonEightTimeDelivery = By.xpath("//li[@data-value='8:00']");
+    public By buttonTimeEndDelivery = By.xpath("//div[@class='MuiSelect-root MuiSelect-select MuiSelect-selectMenu MuiSelect-outlined MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputMarginDense MuiOutlinedInput-inputMarginDense']/following::div[2]");
+    public By buttonSixteenTimeDelivery = By.xpath("//li[@data-value='16:00']");
+    public By buttonTwelveTimeDelivery = By.xpath("//li[@data-value='12:00']");
+    public By buttonNote = By.xpath("//textarea[@placeholder='Примечание']");
+    public By buttonPromoCode = By.xpath("//input[@name='promocode']");
+    public By buttonGoCheckout = By.xpath("//button[@data-testid='go-checkout-btn']");
+    public By buttonSavePromoCode = By.xpath("//div[@data-testid='save-promo']");
+    public By buttonOrder = By.xpath("//a[contains(.,'Заказы')]");
+    public By buttonCarDelivery = By.xpath(" //button[@data-testid='btn-1-delivery']");
+    public By buttonDeletePromoCode = By.xpath("//button[@class='style_promocode_btn_close__rygKX']");
+    public By buttonUponReceipt = By.xpath("//*[@data-testid='option-payment-5']");//При получении
+    public By buttonCardOnline = By.xpath("//*[@data-testid='option-payment-3']");//Картой онлайн
+    public By buttonQrCode = By.xpath("//*[@data-testid='option-payment-4']");//По QR-code
+    public By buttonBill = By.xpath("//*[@data-testid='option-payment-1']");//По Выписать счет
+    public By fieldInn = By.xpath("//input[@data-testid='inn_org']");
+    public By fieldKpp = By.xpath("//input[@data-testid='kpp_org']");
+    public By buttonSetCompany = By.xpath("//button[@data-testid='set-company-button']");
+    public By buttonClosePopup = By.xpath("//button[@data-testid='closePopup']");
+    public By fieldNumberCard = By.xpath("//input[@id='iPAN_sub']");//номер карты
+    public By fieldNumberMonth = By.xpath("//input[@id='input-month']");//номер месяца
+    public By fieldNameHolder = By.xpath("//input[@id='iTEXT']");//имя держателя
+    public By fieldNumberYear = By.xpath("//input[@id='input-year']");//номер года
+    public By fieldNumberCvc = By.xpath("//input[@id='iCVC']");//cvc карты
+    public By buttonSubmitPayment = By.xpath("//button[@id='buttonPayment']");
+    public By fieldPasswordPayment = By.xpath("//input[@id='password']");
 
+    public By buttonBall = By.xpath("//*[@data-testid='option-payment-7']");//баллами
 }
