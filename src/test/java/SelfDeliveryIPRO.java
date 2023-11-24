0e722d822bf667f242e87e55b57496e6c2de8631
Elena Timofeeva <elena.posazh@mail.ru>
2023-11-24 12:54:07 +0300

    added searchig sdek points

--- src/test/java/SelfDeliveryIPRO.java
+++ src/test/java/SelfDeliveryIPRO.java
@@ -7,8 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 
-public class SelfDeliveryIPRO extends CoreTestCase
-{
+public class SelfDeliveryIPRO extends CoreTestCase {
     String SITE_URL = "https://idev.etm.ru/catalog";
     AuthPageObject AuthPageObject = new AuthPageObject();
     MainPageObject MainPageObject = new MainPageObject();
@@ -16,8 +15,7 @@ public class SelfDeliveryIPRO extends CoreTestCase
 
 
     @Test
-    public void ETMSearchPoints() throws InterruptedException
-    {
+    public void ETMSearchPoints() throws InterruptedException {
         driver.get(SITE_URL);
         AuthPageObject.iPROAuthorization();
         MainPageObject.goToDelivery();
@@ -27,5 +25,20 @@ public class SelfDeliveryIPRO extends CoreTestCase
         DeliveryPageObject.SearchAndCheckSelfDelivery("selfdelivery-store-etm-0", "Мыт");
         DeliveryPageObject.SearchAndCheckSelfDelivery("selfdelivery-store-etm-0", "Римск");
         DeliveryPageObject.SearchAndCheckSelfDelivery("selfdelivery-store-etm-0", "Все");
+    }
+
+    @Test
+    public void SdekSearchPoints() throws InterruptedException
+    {
+        driver.get(SITE_URL);
+        AuthPageObject.iPROAuthorization();
+        MainPageObject.goToDelivery();
+        MainPageObject.setSpbInHeader();
+        MainPageObject.waitForElementAndClick("//button[@data-testid='tab-list-selfDelivery']","Не удалось перейти на вкладку Самовывоз", 5);
+        DeliveryPageObject.DisableCheckbox("checkbox-selfdelivery-etm");
+        DeliveryPageObject.SearchAndCheckSelfDelivery("selfdelivery-store-cdeck-0", "Восста");
+        DeliveryPageObject.SearchAndCheckSelfDelivery("selfdelivery-store-cdeck-0", "Куш");
+        DeliveryPageObject.SearchAndCheckSelfDelivery("selfdelivery-store-cdeck-0", "Буда");
+
     }
 }
